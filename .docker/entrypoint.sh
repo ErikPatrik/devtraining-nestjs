dd1678b52cb6a7481a0227bf483f1616d33608b4
erik <erik@meucrediario.com.br>
2022-04-26 15:31:49 -0300

    alteracoes

--- .docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npm install
-npm run build
-npx typeprm migration:run
-npm run start:dev
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.15.4-alpine3.12
+FROM node:16-alpine3.15
 
 RUN apk add --no-cache bash
 
