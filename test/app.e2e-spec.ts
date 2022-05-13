4701cb6c9961878b00ef89eae57fe1b653e36640
erik <erik@meucrediario.com.br>
2022-05-13 09:08:35 -0300

    alteracoes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import * as request from 'supertest'; // pacote npm que fornece uma abstração de alto nível para testar requisições HTTP
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+    let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    // isso aqui vai ser feito antes de cada teste
+    //beforeEach(async () => {
+    beforeAll(async() => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        // A variável App, é uma instância o runtime do nest como o todo
+        // Salva na variável app uma referência da execução, para podemos simular requisições HTTP
+        // Conseguimos simular toda a aplicação
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it('/ (GET)', () => {
+        return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
 });
